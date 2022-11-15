e0b547db1451750c363ae7f415968cc622f1fc03
Sertaç Gültekin <sertacgltkn@gmail.com>
2022-11-15 20:13:47 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import { useState, useEffect } from "react"
-import Loading from "./Loading"
-import Profile from "./Profile"
-
-// const singleUser = `https://api.github.com/users/SankThomas`
-// const repos = `https://api.github.com/users/sankthomas/repos?per_page=5`
-// https://api.github.com/users/sankthomas/repos?page=1&per_page=10&sort=updated
+import { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Profile from "./Profile";
 
 function App() {
-  return <>
-    <h1>hello world</h1>
-  </>
+  const [items, setItems] = useState([]);
+  const [users] = useState("sertacgltkn");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(
+        `https://api.github.com/users/${users}/repos?page=1&per_page=10&sort=updated`
+      );
+      const data = await res.json();
+      setItems(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      {!items ? (
+        <Loading />
+      ) : (
+        <>
+          <section className="py-20 pb-20">
+            <h1 className="text-2xl font-bold">
+              Viewing {users}'s repositories
+            </h1>
+
+            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 mt-10">
+              {items.map((item) => (
+                <Profile key={item.id} {...item} />
+              ))}
+            </div>
+          </section>
+        </>
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/Profile.js
+++ src/Profile.js
@@ -1,9 +1,72 @@
-import React from "react"
+import React from "react";
+import { format } from "date-fns";
 
 export default function Profile(props) {
   return (
     <>
-      <h2>profile</h2>
+      <article className="p-5 bg-white rounded-lg shadow shadow-emerald-300">
+        <img
+          src={props.owner.avatar_url}
+          alt={props.owner.login}
+          className="w-20 h-20 rounded-full shadow"
+        />
+        <article className="flex items-center justify-start">
+          <div className="ml-2">
+            <h2 className="text-lg font-bold capitalize">
+              {props.owner.login}
+            </h2>
+            <p className="text-sm mb-1">{props.name}</p>
+            {props.private ? (
+              <p className="bg-rose-400 py-1 px-2 text-xs text-white shadow rounded-lg inline-block">
+                Private
+              </p>
+            ) : (
+              <p className="bg-emerald-400 py-1 px-2 text-xs text-white shadow rounded-lg inline-block">
+                Public
+              </p>
+            )}
+          </div>
+        </article>
+
+        <div className="my-5">
+          <p>
+            This repository was created on{" "}
+            {format(new Date(props.created_at), "dd MMM yyyy")} by{" "}
+            {props.owner.login}
+          </p>
+        </div>
+        <div className="flex flex-wrap items-center justify-between">
+          <a
+            href={props.html_url}
+            target="_blank"
+            rel="noreferrer"
+            className="underline"
+          >
+            View Repo
+          </a>
+          <ul>
+            <li>{props.stargazers_count.toLocaleString()} stars</li>
+            <li>{props.watchers_count.toLocaleString()} watchers</li>
+          </ul>
+        </div>
+
+        <div className="flex flex-wrap items-center justify-between mt-5">
+          <p className="bg-emerald-400 opacity-75 text-white py-1 px-2 rounded-lg shadow text-xs">
+            {props.language}
+          </p>
+          <ul className="text-right">
+            {props.topics.map((topic, index) => (
+              <li
+                key={index}
+                className="bg-emerald-400 opacity-75 text-white py-1 px-2 rounded-lg shadow text-xs inline-block mx-1"
+              >
+                {topic}
+              </li>
+            ))}
+          </ul>
+          <p className="text-xs">{props.open_issues} issues</p>
+        </div>
+      </article>
     </>
-  )
+  );
 }

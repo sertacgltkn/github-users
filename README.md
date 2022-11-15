0e39697d28fd262aed8fb244156a42838f84add7
Sertaç Gültekin <sertacgltkn@gmail.com>
2022-11-15 18:38:20 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![alt](./preview.png)
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from "react"
+import Loading from "./Loading"
+import Profile from "./Profile"
+
+// const singleUser = `https://api.github.com/users/SankThomas`
+// const repos = `https://api.github.com/users/sankthomas/repos?per_page=5`
+// https://api.github.com/users/sankthomas/repos?page=1&per_page=10&sort=updated
+
+function App() {
+  return <>
+    <h1>hello world</h1>
+  </>
+}
+
+export default App

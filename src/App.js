b631ac71a3de83d24ce5dee4c44f4409f4e3b477
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-26 01:30:33 +0300

    search input available

--- src/App.js
+++ src/App.js
@@ -4,37 +4,52 @@ import Profile from "./Profile";
 
 function App() {
   const [items, setItems] = useState([]);
-  const [users] = useState("sertacgltkn");
+  const [searchTerm, setSearchTerm] = useState("");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch(
-        `https://api.github.com/users/${users}/repos?page=1&per_page=10&sort=updated`
-      );
-      const data = await res.json();
-      setItems(data);
-    };
+  const fetchData = async () => {
+    const res = await fetch(
+      `https://api.github.com/users/${searchTerm}/repos?page=1&per_page=12&sort=updated`
+    );
+    const data = await res.json();
+    setItems(data);
+  };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
     fetchData();
-  }, []);
+  };
 
   return (
     <>
-      {!items ? (
-        <Loading />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Enter a GitHub username"
+        />
+        <button type="submit">Search</button>
+      </form>
+      {!searchTerm ? (
+        <p>Please enter a GitHub username</p>
       ) : (
         <>
-          <section className="py-20 pb-20">
-            <h1 className="text-2xl font-bold">
-              Viewing {users}'s repositories
-            </h1>
-
-            <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 mt-10">
-              {items.map((item) => (
-                <Profile key={item.id} {...item} />
-              ))}
-            </div>
-          </section>
+          {!items ? (
+            <Loading />
+          ) : (
+            <>
+              <section className="py-20 pb-20">
+                <h1 className="text-2xl font-bold">
+                  Viewing {searchTerm}'s repositories
+                </h1>
+                <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 mt-10">
+                  {items.map((item) => (
+                    <Profile key={item.id} {...item} />
+                  ))}
+                </div>
+              </section>
+            </>
+          )}
         </>
       )}
     </>
